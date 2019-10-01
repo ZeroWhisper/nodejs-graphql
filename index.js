64ff3cae4b318896d097175ddfaac28f9a108f67
ZeroWhisper <marcos.devel@gmail.com>
2019-10-01 18:35:18 -0300

    Automatizando projeto

--- index.js
+++ index.js
@@ -1,90 +1,18 @@
-import cors from "cors";
-import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
-import { buildSchema } from "graphql";
+// import cors from 'cors';
+import express from 'express';
+// import { ApolloServer, gql } from 'apollo-server-express';
+// import { buildSchema } from 'graphql';
 
 const app = express();
 // app.use(cors);
 
-// const books = [
-//   {
-//     title: "Harry Potter and the Chamber of Secrets",
-//     author: "J.K. Rowling"
-//   },
-//   {
-//     title: "Jurassic Park",
-//     author: "Michael Crichton"
-//   }
-// ];
-
-// const typeDefs = gql`
-//   type Book {
-//     title: String
-//     author: Author
-//   }
-
-//   type Author {
-//     books: [Book]
-//   }
-
-//   type Query {
-//     author: Author
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     author(parent, args, context, info) {
-//       return [books[0]];
-//     }
-//   },
-//   Author: {
-//     books(author) {
-//       return [books[0]];
-//     }
-//   }
-// };
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    getBooks: [Book]
-    getAuthors: [Author]
-  }
-  type Book {
-    title: String
-    author: Author
-  }
-
-  type Author {
-    name: String
-    books: [Book]
-  }
-  type Mutation {
-    addBook(title: String, author: String): Book
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!"
-  },
-  User: {
-    users: () => ({ name: "TESTE", email: "email" })
-  }
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.applyMiddleware({ app, path: "/graphql" });
-
 const port = 5000;
 
-app.get("/api", (req, res) => {
-  res.send({ status: "ok" });
+app.get('/api', (req, res) => {
+  res.send({ status: 'ok' });
 });
 
 app.listen({ port }, () => {
-  console.log(`Apollo Server on http://localhost:${port}/graphql`);
+  console.log(`Graphql on http://localhost:${port}/graphql`);
+  console.log(`Server on http://localhost:${port}/api`);
 });
--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
+readme: https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+
 # Project Backend
+
+### Fonts
+
+ - https://www.robinwieruch.de/graphql-apollo-server-tutorial (excellent)
+ - https://www.apollographql.com/docs/apollo-server/essentials/server/
+ - https://teamtreehouse.com/library/install-and-use-sequelize-cli
 
 ### Inicializando o projeto
 
@@ -20,22 +28,6 @@ yarn add apollo-server apollo-server-express graphql
 yarn add cors
 ```
 
-======
-
-# OTHERS!!!
-
-### Fonts
-
- - https://www.robinwieruch.de/graphql-apollo-server-tutorial (excellent)
- - https://www.apollographql.com/docs/apollo-server/essentials/server/
-
-
-### Main Packages
-
-yarn add apollo-server apollo-server-express
-yarn add express graphql
-yarn add cors
-
 ### Aux Packages
 
 ```sh
@@ -43,3 +35,45 @@ yarn add -D @babel/core @babel/cli @babel/preset-env @babel/node
 
 yarn add -D sequelize-cli
 ```
+
+### Comandos Sequelize
+
+## Aux
+
+`https://github.com/sequelize/sequelize-auto`
+
+
+## Database
+
+# Se você não tem a database ainda (Cria com base na configuração de conexão)
+`yarn sequelize db:create`
+
+# Migrando os dados de outra forma
+`npx sequelize-cli db:migrate --url 'mysql://root:password@mysql_host.com/database_name'`
+
+
+
+## Model
+
+# Cria novo model (This can be overwritten later with --f flag.)
+ - O comando gera tanto o model quanto o migration
+ - Font: https://sequelize.org/master/manual/migrations.html
+`yarn sequelize model:create --name Student --attributes firstName:string,email:string`
+
+Obs: Note que não há espaço entre os parametros depois de `--attributes`
+
+
+
+## Migrate
+
+# Cria um novo migrate (Gera apenas a Migration)
+`yarn sequelize migration:create --name=create-users`
+
+# Exporta um migrate para o banco de dados
+`yarn sequelize db:migrate`
+
+# Desfaz a ultima exportação
+`yarn sequelize db:migrate:undo`
+
+# Desfaz tudo
+`yarn sequelize db:migrate:undo:all`
